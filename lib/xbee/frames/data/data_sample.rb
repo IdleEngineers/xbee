89640f4e01810d39276be5960caad0408ec95340
Aaron Ten Clay <aarontc@aarontc.com>
2017-05-22 01:23:58 -0700

    Data sample working

---
+++ lib/xbee/frames/data/data_sample.rb
@@ -0,0 +1,73 @@
+# frozen_string_literal: true
+
+module XBee
+	module Frames
+		module Data
+			class DataSample
+				# Bit-mapping of digital channel names
+				DIGITAL_CHANNELS = [
+					:na,
+					:na,
+					:na,
+					:DIO12,
+					:DIO11,
+					:DIO10,
+					:na,
+					:na,
+					:DIO7,
+					:DIO6,
+					:DIO5,
+					:DIO4,
+					:DIO3,
+					:DIO2,
+					:DIO1,
+					:DIO0,
+				]
+
+				# Bit-mapping of analog channel names
+				ANALOG_CHANNELS = [
+					:supply_voltage,
+					:na,
+					:na,
+					:na,
+					:AD3,
+					:AD2,
+					:AD1,
+					:AD0,
+				]
+
+
+				attr_reader :data
+				attr_reader :digital_values
+				attr_reader :analog_values
+
+
+				# Parses the input bytes into @digital_values and @analog_values.
+				# Consumes the bytes read, so the next sample can be constructed.
+				# @param bytes [Array<Integer>] Input data.
+				def initialize(bytes:)
+					input = bytes
+					@digital_channel_mask = (input.shift << 8) + input.shift
+					@analog_channel_mask = input.shift
+
+					@digital_values = {}
+					if @digital_channel_mask > 0
+						raw = (input.shift << 8) + input.shift
+						DIGITAL_CHANNELS.reverse.each_with_index do |channel, index|
+							if (@digital_channel_mask & (1 << index)) > 0
+								@digital_values[channel] = (raw & (1 << index)) > 0 ? 1 : 0
+							end
+						end
+					end
+
+					@analog_values = {}
+					ANALOG_CHANNELS.reverse.each_with_index do |channel, index|
+						if (@analog_channel_mask & (1 << index)) > 0
+							@analog_values[channel] = (input.shift << 8) + input.shift
+						end
+					end
+				end
+			end
+		end
+	end
+end
--- lib/xbee/frames/zig_bee_io_data_sample_rx_indicator.rb
+++ lib/xbee/frames/zig_bee_io_data_sample_rx_indicator.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require_relative 'frame'
+require_relative 'data/data_sample'
 
 module XBee
 	module Frames
@@ -7,8 +8,27 @@ module XBee
 			api_id 0x92
 
 
-			def initialize(packet: nil)
+			# [Array<Data::DataSample>] Array of sample objects
+			attr_reader :samples
 
+			def initialize(packet: nil)
+				@samples = []
+
+				super
+				# Receive options: 01
+				# Number of samples: 01
+				# Digital channel mask: 00 10
+				# Analog channel mask: 00
+				# DIO4/AD4 digital value: Low
+				# Checksum: 4A
+				if data
+					input = data.dup
+					@number_of_samples = input.shift
+
+					@number_of_samples.times do
+						@samples << Data::DataSample.new(bytes: input)
+					end
+				end
 			end
 		end
 	end
